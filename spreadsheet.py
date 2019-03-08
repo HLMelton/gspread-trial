4ef41f016f88f4a7061b7aa5184ea76c21bd0553
Hunter L. Melton <hunterlmelton@gmail.com>
2019-03-08 13:17:23 -0800

    AuthKeyHidden

--- spreadsheet.py
+++ spreadsheet.py
@@ -2,7 +2,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ['https://spreadsheets.google.com/feeds']
-creds = ServiceAccountCredentials.from_json_keyfile_name('Legislators-343067072d37.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 # Json file can be renamed, though it needs to be edited here as well.
 # The current name is the dafault given to it by GoogleAPIs
 
