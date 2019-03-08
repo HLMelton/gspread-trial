a2d83b96e9d1a62790f8dd0eb303c64bf42e6ffa
Hunter L. Melton <hunterlmelton@gmail.com>
2019-03-08 13:14:32 -0800

    Initial commit

---
+++ spreadsheet.py
@@ -0,0 +1,15 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds']
+creds = ServiceAccountCredentials.from_json_keyfile_name('Legislators-343067072d37.json', scope)
+# Json file can be renamed, though it needs to be edited here as well.
+# The current name is the dafault given to it by GoogleAPIs
+
+
+client = gspread.authorize(creds)
+
+sheet = client.open('Legislators 2017').sheet1
+
+legislators = sheet.get_all_records()
+print(legislators)
